ded9abdf8b5071ac98d1d88b09a0d8d781c7c88e
Jeriann <Jeriann1990@gmail.com>
2017-11-01 19:28:26 -0500

    musicgrapgh api key added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
       
 
       var searchQuery = "Michael Jackson";
-      var queryURL = "http://api.musicgraph.com/api/v2/artist/search?api_key=&name=" + searchQuery;
+      var queryURL = "http://api.musicgraph.com/api/v2/artist/search?api_key=4db32eb564d567abea9870b5e9381c4b&name=" + searchQuery;
 
       $.ajax({
           url: queryURL,
