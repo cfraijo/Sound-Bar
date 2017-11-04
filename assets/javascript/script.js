25852393a9c4afd50de15c92052610974fce3907
Christian Fraijo <ChristianFraijo@Christians-MacBook-Pro.local>
2017-11-04 11:34:36 -0500

    fixed Music API request

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,7 +6,11 @@ $(document).ready(function(){
 
       var queryURL2 = "http://api.musicgraph.com/api/v2/artist/search?api_key=4db32eb564d567abea9870b5e9381c4b&similar_to=" + searchQuery + "&limit=10";
 
-      var queryURL3 = "http://app.ticketmaster.com/discovery/v1/events.json?keyword=Michael+Jackson&apikey=s12kNXNClYbfqtRG3dikwAp75zpbZm7i";
+      var queryURL3 = "http://app.ticketmaster.com/discovery/v1/events.json?keyword=" + searchQuery + "&apikey=s12kNXNClYbfqtRG3dikwAp75zpbZm7i";
+
+      var queryURL4 = "http://api.musicgraph.com/api/v2/album/search?api_key=4db32eb564d567abea9870b5e9381c4b&artist_name=" + searchQuery + "&limit=10";
+
+      
 
       $.ajax({
           url: queryURL,
@@ -30,6 +34,8 @@ $(document).ready(function(){
 
           			console.log(results2[i].name);
 
+                $("#similar").append("<div>" + results2[i].name + "</div>");
+
           		}
           
         });
@@ -39,15 +45,42 @@ $(document).ready(function(){
           method: "GET"
         })
               .done(function(response) {
-          		var results3 = response._embedded;
+          		var results3 = response._embedded.events;
 
-          		
+              var venue = "_embedded.venue.name";
 
-          			console.log(results3);
+              var venueDate = "_embedded.venue.dates.start.localDate";
 
-          		
+              console.log(results3[0].venue);
+              console.log(results3[0].venueDate);
+
+          		for(var i = 0; i < 10; i++) {
+
+          			   console.log("results: " + results3[i].venue);
+
+                   $("#tour-dates").append("<div>" + results3[i].name + "</div>");
+                   $("#tour-dates").append("<div>" + results3[i].venue + "</div>");
+                   $("#tour-dates").append("<div>" + results3[i].venueDate + "</div>");
+                }
           
         });
+
+       $.ajax({
+          url: queryURL4,
+          method: "GET"
+        })
+              .done(function(response) {
+              var results4 = response.data;
+
+              for(var i = 0; i < results4.length; i++){
+
+                console.log(results4[i].title);
+
+                $("#albums").append("<div>" + results4[i].title + "</div>");
+
+              }
+          
+        });       
 
 
 });
