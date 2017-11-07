af236af64319af7cfaff89ce11970c74d93cb846
Christian Fraijo <ChristianFraijo@Christians-MacBook-Pro.local>
2017-11-06 19:05:51 -0600

    added on click event and set value of search box as api query

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,95 +1,103 @@
 $(document).ready(function(){  
 
 
-    $('.center').slick({
+    $(".center").slick({
         infinite: true,
         slidesToShow: 5,
         slidesToScroll: 1,
       });
 
-      var searchQuery = "Michael Jackson";
+      var searchQuery, queryURL, queryURL2, queryURL3, queryURL4;
       
-      var queryURL = "http://api.musicgraph.com/api/v2/artist/search?api_key=4db32eb564d567abea9870b5e9381c4b&name=" + searchQuery + "&limit=1";
 
-      var queryURL2 = "http://api.musicgraph.com/api/v2/artist/search?api_key=4db32eb564d567abea9870b5e9381c4b&similar_to=" + searchQuery + "&limit=10";
+      $("#button").on("click", function(event){
 
-      var queryURL3 = "http://app.ticketmaster.com/discovery/v1/events.json?keyword=" + searchQuery + "&apikey=s12kNXNClYbfqtRG3dikwAp75zpbZm7i";
+        event.preventDefault();
 
-      var queryURL4 = "http://api.musicgraph.com/api/v2/album/search?api_key=4db32eb564d567abea9870b5e9381c4b&artist_name=" + searchQuery + "&limit=10";
+        searchQuery = $("#search-box").val().trim();
+
+        queryURL = "http://api.musicgraph.com/api/v2/artist/search?api_key=4db32eb564d567abea9870b5e9381c4b&name=" + searchQuery + "&limit=1";
+
+        queryURL2 = "http://api.musicgraph.com/api/v2/artist/search?api_key=4db32eb564d567abea9870b5e9381c4b&similar_to=" + searchQuery + "&limit=10";
+
+        queryURL3 = "http://app.ticketmaster.com/discovery/v1/events.json?keyword=" + searchQuery + "&apikey=s12kNXNClYbfqtRG3dikwAp75zpbZm7i";
+
+        queryURL4 = "http://api.musicgraph.com/api/v2/album/search?api_key=4db32eb564d567abea9870b5e9381c4b&artist_name=" + searchQuery + "&limit=10";
 
 
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-              .done(function(response) {
-          		var results = response.data;
+          $.ajax({
+              url: queryURL,
+              method: "GET"
+            })
+                  .done(function(response) {
+              		var results = response.data;
 
-          		console.log(results[0].name);
-          
+              		console.log(results[0].name);
+              
+            });
+
+          $.ajax({
+              url: queryURL2,
+              method: "GET"
+            })
+                  .done(function(response) {
+              		var results2 = response.data;
+
+              		for(var i = 0; i < results2.length; i++){
+
+              			console.log(results2[i].name);
+
+
+                    $("#similar").append("<div>" + results2[i].name + "</div>");
+
+              		}
+              
+            });
+
+          $.ajax({
+              url: queryURL3,
+              method: "GET"
+            })
+                  .done(function(response) {
+
+              		var results3 = response._embedded.events;
+
+                  var venue = "_embedded.venue.city";
+
+                  var venueDate = "dates.start.localDate";
+
+                  console.log(results3[0].venue);
+                  console.log(results3[0].dates.start.localDate);
+
+              		for(var i = 0; i < 5; i++) {
+
+              			   console.log("results: " + results3[i].venue);
+
+                       $("#tour-dates").append("<div>" + results3[i].name + "</div>");
+                       $("#tour-dates").append("<div>" + results3[i]._embedded.venue[0].city.name + "</div>");
+                       $("#tour-dates").append("<div>" + results3[i].dates.start.localDate + "</div>");
+                    }
+              
+            });
+
+           $.ajax({
+              url: queryURL4,
+              method: "GET"
+            })
+                  .done(function(response) {
+                  var results4 = response.data;
+
+                  for(var i = 0; i < results4.length; i++){
+
+                    console.log(results4[i].title);
+
+                    $("#albums").append("<div>" + results4[i].title + "</div>");
+
+                  }
+              
+            });       
+
+
         });
-
-      $.ajax({
-          url: queryURL2,
-          method: "GET"
-        })
-              .done(function(response) {
-          		var results2 = response.data;
-
-          		for(var i = 0; i < results2.length; i++){
-
-          			console.log(results2[i].name);
-
-
-                $("#similar").append("<div>" + results2[i].name + "</div>");
-
-          		}
-          
-        });
-
-      $.ajax({
-          url: queryURL3,
-          method: "GET"
-        })
-              .done(function(response) {
-
-          		var results3 = response._embedded.events;
-
-              var venue = "_embedded.venue.city";
-
-              var venueDate = "dates.start.localDate";
-
-              console.log(results3[0].venue);
-              console.log(results3[0].dates.start.localDate);
-
-          		for(var i = 0; i < 10; i++) {
-
-          			   console.log("results: " + results3[i].venue);
-
-                   $("#tour-dates").append("<div>" + results3[i].name + "</div>");
-                   $("#tour-dates").append("<div>" + results3[i]._embedded.venue[0].city.name + "</div>");
-                   $("#tour-dates").append("<div>" + results3[i].dates.start.localDate + "</div>");
-                }
-          
-        });
-
-       $.ajax({
-          url: queryURL4,
-          method: "GET"
-        })
-              .done(function(response) {
-              var results4 = response.data;
-
-              for(var i = 0; i < results4.length; i++){
-
-                console.log(results4[i].title);
-
-                $("#albums").append("<div>" + results4[i].title + "</div>");
-
-              }
-          
-        });       
-
-
 
 });
